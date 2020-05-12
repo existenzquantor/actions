21891e8050e7df13d53ed9a74296f317fdde7fa7
Felix Lindner <felix@existenzquantor.info>
2020-05-12 20:36:45 +0200

    Added Agenda

--- actions.go
+++ actions.go
@@ -24,16 +24,7 @@ func reasonAction(action string, causalitypath *string, c string, d string) mode
 	return model.ParsePrologOutput(string(b))
 }
 
-func main() {
-	jsonFile := flag.String("json", "./ressources/flipSwitch.json", "JSON file that contains a domain description.")
-	causalitypath := flag.String("causalitypath", "../causality/", "Path to executable of causal reasoning")
-
-	flag.Parse()
-
-	m := model.ParseDomainJSON(*jsonFile)
-	c := model.ToCausalityOutput(m)
-	d := model.OutputProgram(m)
-
+func actionConcepts(m model.DomainDescription, c string, d string, causalitypath *string) model.ActionConcepts {
 	var concepts []model.ActionConcept
 	for i := 0; i < len(m.ProgramDescription.ActionSequence); i++ {
 		a := m.ProgramDescription.ActionSequence[i]
@@ -42,9 +33,33 @@ func main() {
 		n := model.ActionConcept{ActionName: a, Context: s, Causes: o}
 		concepts = append(concepts, n)
 	}
-	ac, err := json.Marshal(model.ActionConcepts{Concepts: concepts})
-	if err != nil {
-		log.Fatal(err)
+	return model.ActionConcepts{Concepts: concepts}
+}
+
+func main() {
+	jsonFile := flag.String("domain", "./ressources/flipSwitch.json", "JSON file that contains a domain description.")
+	causalitypath := flag.String("causalitypath", "../causality/", "Path to the executable of causal reasoning, see https://github.com/existenzquantor/causality")
+	ontology := flag.String("ontology", "https://github.com/existenzquantor/actions/ressources/FlipSwitch.owl", "IRI of the Ontology to use")
+	outputformat := flag.String("outputformat", "types", "types | concepts")
+	hermitpath := flag.String("hermitpath", "../../reasoner/HermiT/", "Path to the HermiT OWL reasoner")
+
+	flag.Parse()
+
+	m := model.ParseDomainJSON(*jsonFile)
+	c := model.ToCausalityOutput(m)
+	d := model.OutputProgram(m)
+
+	switch *outputformat {
+	case "concepts":
+		ac, err := json.Marshal(actionConcepts(m, c, d, causalitypath))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%v\n", string(ac))
+	case "types":
+		fmt.Printf("TODO:\nWrite new temp.owl that imports %v\n", ontology)
+		fmt.Printf("Add new concepts built from %v\n", actionConcepts(m, c, d, causalitypath))
+		fmt.Printf("Invoke: %vjava -jar HermiT.jar -c temp.owl\n", hermitpath)
+		fmt.Printf("Finally parse hermit's output and extract types\n")
 	}
-	fmt.Printf("%v\n", string(ac))
 }
